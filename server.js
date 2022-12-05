eca9e47302ece237b5ed4f8dbab5f98b662fad6d
wangzetao <wangzhazha21@gmail.com>
2022-12-05 15:32:12 +0800

    修复字典键值导致的排序错误bug 修改排行榜存储方式数组

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 /*
 数据格式:
-{
-    "hello": 18446744073709551616
-    "13A": 2147483647,
-    "AI": 1,
-}
+[
+    {
+        name: "hello",
+        score: 18446744073709551616
+    },
+    {
+        name: "13A", 
+        score: 2147483647,
+    },
+    {
+        name: "AI",
+        score: 1
+    }
+]
 */
-if(!fs.existsSync("data.json")) fs.writeFileSync("data.json", "{}");
+if(!fs.existsSync("data.json")) fs.writeFileSync("data.json", "[]");
 
 var data = JSON.parse(fs.readFileSync("data.json"));
 
@@ -28,31 +37,43 @@ app.get('/', function (req, res) {
 如果a应该在b后面，就返回一个正数。
 */
 function comp(a, b) {
-    return data[b] - data[a];
+    return b.score - a.score;
 }
 
-function sort(dict) {
-    var new_keys = Object.keys(dict).sort(comp);
-    var new_dict = {};
-    for (var k = 0; k < new_keys.length; k++) {
-        new_dict[new_keys[k]] = dict[new_keys[k]];
-    }
-    return new_dict;
-}
+// function sort(data) {
+//     var new_keys = Object.keys(dict).sort(comp);
+//     var new_dict = {};
+//     for (var k = 0; k < new_keys.length; k++) {
+//         new_dict[new_keys[k]] = dict[new_keys[k]];
+//     }
+//     return new_dict;
+// }
 
 app.post('/get', function (req, res) {
     res.end(JSON.stringify(data));
 });
 
+function updateData(){
+    data = data.sort(comp);
+    fs.writeFile("data.json", JSON.stringify(data), (err) => { });
+}
+
 app.post('/set', urlencodedParser, function (req, res) {
     var name = req.body.name;
     var score = req.body.score;
-    if (data[name] == undefined || data[name] < score) {
-        data[name] = score;
-        console.log(data);
-        data = sort(data);
-        fs.writeFile("data.json", JSON.stringify(data), (err) => { });
-        console.log(data);
+    var index = -1;
+    for(var i=0;i<data.length;i++){
+        if(data[i].name == name) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1){
+        data.push({name: name, score: score});
+        updateData();
+    } else if (data[index].score < score){
+        data[index].score = score;
+        updateData();
     }
     res.end(JSON.stringify(data));
 });
